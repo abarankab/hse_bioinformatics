ec02e6b331c43fab9dfcd712e5763c5c56d542d9
Anthony Baryshnikov <anthony.baryshnikov@gmail.com>
2020-11-26 15:37:18 +0300

    HW 3

--- HW_2/analyze_bootstrap_values.py
+++ HW_2/analyze_bootstrap_values.py
@@ -3,7 +3,7 @@ from statistics import mean, median
 values = list(map(int, input().split()))
 values.sort()
 
-smallest_size = 5
+smallest_size = 3
 
 print(f"Average of {smallest_size} smallest values: {mean(values[:smallest_size])}")
 print(f"Average value: {mean(values)}")
